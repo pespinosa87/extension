21205a2178f90a73cb70966566332621b728ae45
pespinosa87 <pespinosa@prensaiberica.es>
2025-04-04 11:02:02 +0200

    Forzado: API corregida manualmente

--- controllers/api.py
+++ controllers/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from models.medio import get_all_medios, add_medio
-from models.tema import get_temas, get_temas_por_dominio
+from models.tema import get_temas_por_dominio
 from services.scanner import escanear_medios_por_lotes as escanear_todos_los_medios, agregar_medios_prensa
 from threading import Thread
 
