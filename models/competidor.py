8cb9ceead58e9f1303e0fe3f22c948e80e44737f
pespinosa87 <pespinosa@prensaiberica.es>
2025-04-07 11:44:18 +0200

    Gestión de competidores-ext2

--- models/competidor.py
+++ models/competidor.py
@@ -1,5 +1,32 @@
 import psycopg2
+import psycopg2.extras
 from config import Config
+
+def get_db_connection():
+    return psycopg2.connect(Config.DATABASE)
+
+def add_competidor(medio_competidor_id, medio_padre_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO competidores (medio_competidor_id, medio_padre_id)
+        VALUES (%s, %s)
+        ON CONFLICT DO NOTHING
+    """, (medio_competidor_id, medio_padre_id))
+    conn.commit()
+    conn.close()
+
+def get_competidores_por_medio_padre(medio_padre_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("""
+        SELECT m.* FROM competidores c
+        JOIN medios m ON c.medio_competidor_id = m.id
+        WHERE c.medio_padre_id = %s
+    """, (medio_padre_id,))
+    resultados = cursor.fetchall()
+    conn.close()
+    return resultados
 
 def get_competidores_relacionados(dominio_actual):
     conn = psycopg2.connect(Config.DATABASE)
@@ -29,3 +56,4 @@ def get_competidores_relacionados(dominio_actual):
 
     conn.close()
     return [{"nombre": nombre, "url": url} for nombre, url in resultados]
+
