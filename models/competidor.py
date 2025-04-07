b91ffe33ddbe34ccf0d520b5b41c593ed314b9f1
pespinosa87 <pespinosa@prensaiberica.es>
2025-04-07 11:39:38 +0200

    Gestión de competidores-ext

--- models/competidor.py
+++ models/competidor.py
@@ -1,29 +1,31 @@
 import psycopg2
-import psycopg2.extras
 from config import Config
 
-def get_db_connection():
-    return psycopg2.connect(Config.DATABASE)
-
-def add_competidor(medio_competidor_id, medio_padre_id):
-    conn = get_db_connection()
+def get_competidores_relacionados(dominio_actual):
+    conn = psycopg2.connect(Config.DATABASE)
     cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO competidores (medio_competidor_id, medio_padre_id)
-        VALUES (%s, %s)
-        ON CONFLICT DO NOTHING
-    """, (medio_competidor_id, medio_padre_id))
-    conn.commit()
-    conn.close()
 
-def get_competidores_por_medio_padre(medio_padre_id):
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    # Buscar medio propio cuyo dominio coincida
     cursor.execute("""
-        SELECT m.* FROM competidores c
+        SELECT id FROM medios
+        WHERE tipo = 'propio' AND %s ILIKE CONCAT('%%', REPLACE(REPLACE(REPLACE(REPLACE(url, 'https://', ''), 'http://', ''), 'www.', ''), '/', ''), '%%')
+    """, (dominio_actual,))
+    medio = cursor.fetchone()
+
+    if not medio:
+        conn.close()
+        return []
+
+    medio_id = medio[0]
+
+    # Buscar competidores asociados
+    cursor.execute("""
+        SELECT m.nombre, m.url
+        FROM competidores c
         JOIN medios m ON c.medio_competidor_id = m.id
         WHERE c.medio_padre_id = %s
-    """, (medio_padre_id,))
+    """, (medio_id,))
     resultados = cursor.fetchall()
+
     conn.close()
-    return resultados
+    return [{"nombre": nombre, "url": url} for nombre, url in resultados]
